1b56e854b70b5827dac24d28cb70f394dde133bf
QAnhTran <tquangnh87@gmail.com>
2023-08-06 22:18:07 +0700

    Add delete task function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const submitButton = document.querySelector("button");
+const input = document.getElementById("userTask");
 let task;
+
 
 submitButton.addEventListener("click", function() {
     let view = document.getElementById("taskList");
@@ -8,8 +10,25 @@ submitButton.addEventListener("click", function() {
 	var node = document.createTextNode(task);
 	newTask.appendChild(node);
 	view.appendChild(newTask);
+	input.value = "";
+
     
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	dBtn.classList.add("button");
+	newTask.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteTask);
+	// END ADD DELETE BUTTON
+
+
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteTask(){
+		newTask.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
+
     console.log(task);
 });
+
 
 console.log(task);
